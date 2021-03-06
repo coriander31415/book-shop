23c2ccb3e94e4d245c3c2772d79e769dea2b227e
coriander31415 <coriander31415@gmail.com>
2021-03-06 10:23:06 +0200

    feat: add app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'bookShop';
+export class AppComponent implements OnInit {
+  @ViewChild('appTitle', { static: false }) appTitle!: ElementRef;
+
+  bookShopTitle = 'Book Shop';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.appTitle.nativeElement.textContent = this.bookShopTitle;
+    }, 0);
+  }
 }
